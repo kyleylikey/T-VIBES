d8ae4ff0a362ef386ff88842de74322a2a66423e
Effcem <franzlester.medina.cics@ust.edu.ph>
2025-01-21 02:36:15 +0800

    Added Form Validation in the Sign Up Page (Fullname, Username, Contact No., and Password)

--- src/views/frontend/signup.php
+++ src/views/frontend/signup.php
@@ -13,12 +13,12 @@
         <p>Please register to log in.</p>
         <form action="../../controllers/signupcontroller.php" method="POST">
             <div class="form-group">
-                <input type="text" id="fullname" name="fullname" placeholder="Full Name" required class="half-width-input">
-                <input type="text" id="username" name="username" placeholder="Username" required class="half-width-input">
+            <input type="text" id="fullname" name="fullname" placeholder="Full Name" required class="half-width-input" pattern="^[A-Za-z\s]+$" title="Full name must contain only alphabets and spaces.">
+                <input type="text" id="username" name="username" placeholder="Username" required class="half-width-input" pattern="^\w{3,20}$" title="Username must be 3-20 characters long and can only include letters, numbers, and underscores.">
             </div>
-            <input type="tel" id="contact" name="contact" placeholder="Contact Number" required class="full-width-input">
+            <input type="tel" id="contact" name="contact" placeholder="Contact Number" pattern="^\d{11}$" required class="full-width-input" title="Contact number must be exactly 11 digits." inputmode="numeric" oninput="this.value = this.value.replace(/[^0-9]/g, '');">
             <input type="email" id="email" name="email" placeholder="Email" required class="full-width-input">            
-            <input type="password" id="password" name="password" placeholder="Password" required class="full-width-input">
+            <input type="password" id="password" name="password" placeholder="Password" required class="full-width-input" pattern="^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$" title="Password must be at least 8 characters long, and include one letter, one number, and one special character.">
 
             <div class="checkbox-container">
                 <input type="checkbox" id="privacyPolicy" name="privacyPolicy" required>
