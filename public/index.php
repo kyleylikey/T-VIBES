e826fcdb61fb0839e0c63c5e3fdc81a69dc22259
Kyle Ashleigh Baldoza <baldozakyle15@gmail.com>
2025-01-28 18:19:40 +0800

    Added header template. Location: src/views/templates/header.php

--- public/index.php
+++ public/index.php
@@ -4,9 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="assets/styles/main.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 </head>
 <body>
-    success
-    <!-- this is the homepage. -->
+    <?php include '../src/views/templates/header.php'; ?>
 </body>
 </html>
---
+++ src/views/templates/header.php
@@ -0,0 +1,24 @@
+<?php
+$current_page = basename($_SERVER['PHP_SELF']);
+?>
+<style>
+    .active, .nav:hover {
+        font-weight: bold;
+    }
+
+
+</style>
+<header>
+    <div>
+        <nav>
+            <span style="margin-right: 16px;"> *Logo* </span>
+            <a href="/T-VIBES/public/" style="margin-right: 16px;" class="nav <?php echo $current_page == 'index.php' ? 'active' : ''; ?>">Explore</a>
+            <a href="#about" style="margin-right: 16px;" class="nav <?php echo $current_page == 'about.php' ? 'active' : ''; ?>">About Us</a>
+            <a href="#contact" class="nav <?php echo $current_page == 'contact.php' ? 'active' : ''; ?>">Contact</a>
+        </nav>
+        <div>
+            <a href="#tours" class="btn" style="margin-right: 12px;"><i class="<?php echo $current_page == 'tourrequest.php' ? 'bi bi-map-fill' : 'bi bi-map'; ?>"></i></a>
+            <a href="#account" class="btn"><i class="<?php echo $current_page == 'tourrequest.php' ? 'bi bi-person-circle-fill' : 'bi bi-person-circle'; ?>""></i></a>
+        </div>
+    </div>
+</header>
