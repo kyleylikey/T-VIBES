41a32be2ec0de50d9837abbd1642ab039c90de82
Effcem <franzlester.medina.cics@ust.edu.ph>
2025-01-31 18:13:16 +0800

    Added title at verify.php

--- src/controllers/verify.php
+++ src/controllers/verify.php
@@ -37,6 +37,9 @@ if (isset($_GET['email'])) {
     echo "<!DOCTYPE html>
     <html>
     <head>
+        <meta charset='UTF-8'>
+        <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+        <title>Verify Email Address</title>
         <link rel='stylesheet' href='../../public/assets/styles/main.css'>
         <link rel='stylesheet' href='../../public/assets/styles/login.css'>
         <script src='https://cdn.jsdelivr.net/npm/sweetalert2@11'></script>
