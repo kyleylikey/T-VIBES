9c788ff48e2fca2d39cf44180ca7b6a0a2ceaf67
Kyle Ashleigh Baldoza <baldozakyle15@gmail.com>
2025-05-07 19:34:19 +0800

    Changed fetch path for signupcontroller

--- src/views/frontend/signup.php
+++ src/views/frontend/signup.php
@@ -196,7 +196,7 @@ error_reporting(E_ALL);
     //     submitButton.disabled = true;
     //     submitButton.textContent = "Loading...";
 
-    //     fetch('../../controllers/signupcontroller.php', {
+    //     fetch('/src/controllers/signupcontroller.php', {
     //         method: 'POST',
     //         body: formData
     //     })
@@ -248,7 +248,7 @@ error_reporting(E_ALL);
     submitButton.disabled = true;
     submitButton.textContent = "Loading...";
 
-    fetch('../../controllers/signupcontroller.php', {
+    fetch('https://tourtaal.azurewebsites.net/src/controllers/signupcontroller.php', {
         method: 'POST',
         body: formData
     })
