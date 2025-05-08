48277ab8c576b260ab36ba508b15b7a77a6416bf
Kyle Ashleigh Baldoza <baldozakyle15@gmail.com>
2025-05-09 03:26:36 +0800

    added taaltourismdb as db then schema

--- src/controllers/verify.php
+++ src/controllers/verify.php
@@ -13,7 +13,7 @@ if (isset($_GET['token'])) {
 
 
     try {
-        $query = "SELECT * FROM [users] WHERE emailveriftoken = :token AND status = 'inactive' AND token_expiry > GETDATE()";
+        $query = "SELECT * FROM [taaltourismdb].[taaltourismdb].[users] WHERE emailveriftoken = :token AND status = 'inactive' AND token_expiry > GETDATE()";
         $stmt = $conn->prepare($query);
         $stmt->bindParam(':token', $token);
         $stmt->execute();
@@ -24,7 +24,7 @@ if (isset($_GET['token'])) {
         $debugInfo = '';
 
         if ($stmt->rowCount() > 0) {
-            $updateQuery = "UPDATE [users] SET status = 'active', emailveriftoken = NULL, token_expiry = NULL WHERE emailveriftoken = :token";
+            $updateQuery = "UPDATE [taaltourismdb].[taaltourismdb].[users] SET status = 'active', emailveriftoken = NULL, token_expiry = NULL WHERE emailveriftoken = :token";
             $updateStmt = $conn->prepare($updateQuery);
             $updateStmt->bindParam(':token', $token);
 
