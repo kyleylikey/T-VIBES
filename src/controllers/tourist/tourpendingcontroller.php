18c7a60c37ad1a04920ca4572a0f1fbd0e58d995
Kyle Ashleigh Baldoza <baldozakyle15@gmail.com>
2025-05-08 16:51:49 +0800

    added checkpoints for tourpending controller

--- src/controllers/tourist/tourpendingcontroller.php
+++ src/controllers/tourist/tourpendingcontroller.php
@@ -1,15 +1,25 @@
 <?php
-if (session_status() === PHP_SESSION_NONE) {
-    session_start();
-}
+echo "Checkpoint 1: Controller loaded.<br>";
 
 require_once __DIR__ . '/../../config/dbconnect.php';
+echo "Checkpoint 2: DB Connect included.<br>";
+
 require_once __DIR__ . '/../../models/Tour.php';
+echo "Checkpoint 3: Tour model included.<br>";
 
 $database = new Database();
 $db = $database->getConnection();
+echo "Checkpoint 4: DB connection established.<br>";
 
 $tourModel = new Tour($db);
-$userPendingTour = $tourModel->getPendingTourByUser($_SESSION['userid']);
+echo "Checkpoint 5: Tour model instantiated.<br>";
+
+if (isset($_SESSION['userid'])) {
+    echo "Checkpoint 6: User ID is " . $_SESSION['userid'] . "<br>";
+    $userPendingTour = $tourModel->getPendingTourByUser($_SESSION['userid']);
+    echo "Checkpoint 7: Pending tour fetched.<br>";
+} else {
+    echo "Checkpoint 6: User ID not set in session.<br>";
+}
 
 ?>
